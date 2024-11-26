bd38c3019412afdb53ee394e2dd8ccc9294b83a3
fabianegli <fabian.egli@protonmail.ch>
2024-11-26 09:12:11 +0100

    specify file encodings in tests

--- code/tests.py
+++ code/tests.py
@@ -10,12 +10,12 @@ from dark_visitors import json_to_txt, json_to_table
 
 
 def test_robots_txt_creation():
-    robots_json = json.loads(Path("test_files/robots.json").read_text())
+    robots_json = json.loads(Path("test_files/robots.json").read_text(encoding="utf-8"))
     robots_txt = json_to_txt(robots_json)
-    assert Path("test_files/robots.txt").read_text() == robots_txt
+    assert Path("test_files/robots.txt").read_text(encoding="utf-8") == robots_txt
 
 
 def test_table_of_bot_metrices_md():
-    robots_json = json.loads(Path("test_files/robots.json").read_text())
+    robots_json = json.loads(Path("test_files/robots.json").read_text(encoding="utf-8"))
     robots_table = json_to_table(robots_json)
-    assert Path("test_files/table-of-bot-metrics.md").read_text() == robots_table
+    assert Path("test_files/table-of-bot-metrics.md").read_text(encoding="utf-8") == robots_table
